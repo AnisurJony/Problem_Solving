e52cd63641d049889174964feade56ba4ac8827d
AnisurJony <anisurjony182612@gmail.com>
2025-07-23 01:28:45 +0600

    odd or even in c

---
+++ oddOrEven.c
@@ -0,0 +1,19 @@
+#include <studio.h>
+#include <stdbool.h>
+
+bool isEven(int n) {
+    return n%2 == 0;
+
+}
+
+// Driver code
+int main(){
+    int n = 15;
+
+    if (iEven(n))
+        printf("true\n");
+    else 
+        printf("false\n"); 
+    
+    return 0;
+}
