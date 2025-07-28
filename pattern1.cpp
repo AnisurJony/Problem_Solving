9894ddb44a9c3679fdbd3888080f3a067b83850c
AnisurJony <anisurjony182612@gmail.com>
2025-07-29 02:47:23 +0600

    patter2

--- pattern1.cpp
+++ pattern1.cpp
@@ -12,6 +12,18 @@ int n = 4;
             num++;
         }
         cout<<endl<<" ";
-    }   
+    } 
+
+    cout<<endl;
+    for(int i=0; i<n; ++i){
+      
+        for(int j=0; j<n; ++j){
+            cout<<"*";
+            
+        }
+        cout<<endl;
+    }
+
+
     return 0;
 }
